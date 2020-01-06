b5d45d0f12bfd92f324d2256bcdcf89f55a04832
40176468 <acunningham43@qub.ac.uk>
2020-01-06 16:29:04 +0000

    Update to request adverts from the ad service

--- search.py
+++ search.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, redirect, send_file, render_template
+from flask import Flask, request, render_template
 import mysql.connector
 import os
 import string
+import requests
+import json
 
 app = Flask(__name__, template_folder="templates/")
 
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-
+ADVERT_SERVICE_URL = os.environ.get("ADVERT_SERVICE_URL")
 
 db_connection = mysql.connector.connect(
     host=os.environ.get("MYSQL_DB_HOST"),
@@ -27,7 +27,6 @@ def search_results(keywords):
     cursor = db_connection.cursor()
     try:
         individual_keywords = str(keywords).translate(str.maketrans('', '', string.punctuation)).split()
-        print(individual_keywords)
         s_string = ""
         for count in range(len(individual_keywords)):
             s_string = s_string + "%s"
@@ -42,11 +41,14 @@ def search_results(keywords):
         else:
             # display results
             results = list(map(lambda result: result[0], results))
-            print(results)
-            return render_template("index.html", results=results)
+            try:
+                adverts = json.loads(requests.post(url=ADVERT_SERVICE_URL, json=json.dumps(individual_keywords)).content)
+                return render_template("index.html", results=results, adverts=adverts if adverts is not None else ["No adverts found"])
+            except requests.exceptions.ConnectionError:
+                return render_template("index.html", results=results, adverts=["Advert service could not be reached"])
     except mysql.connector.Error as err:
         print("Error: {}".format(err))
         return "Error: {}".format(err)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5025)
